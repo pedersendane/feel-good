e4fcab05508b2977cf03bc5728341221d8c92e7d
Dane Pedersen <dane@Danes-MBP.attlocal.net>
2021-05-18 19:48:41 -0500

    Changed redirect uri

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const redirect = window.location.origin + '/feel-good';
 
 ReactDOM.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
-    redirectUri={window.location.origin}>
+    redirectUri={redirect}>
     <App />
   </Auth0Provider>,
   document.getElementById('root')
